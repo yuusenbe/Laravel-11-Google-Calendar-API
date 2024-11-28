0309c245b1995a38e3164c7b575ad2acb8d2a7ca
yuusenbe <165738616+yuusenbe@users.noreply.github.com>
2024-11-28 15:39:10 +0800

    remove Holiday.php

--- app/Models/Holiday.php
+++ app/Models/Holiday.php
@@ -4,10 +4,3 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class Holiday extends Model
-{
-    use HasFactory;
-
-    protected $fillable = ['date', 'description', 'regions']; 
-}
